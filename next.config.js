8546c97bfcaf83d3f0fef9d0a6d0c68e864bd655
Oleksii Rozhko <rozhkoaleks@gmail.com>
2025-03-09 17:13:58 +0200

    Update Next.js image configuration with remote patterns
    - Replace deprecated domains configuration with remotePatterns
    - Maintain support for i.ibb.co and s3-eu-west-1.amazonaws.com image sources
    - Preserve existing image formats, device sizes, and image sizes

--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
 module.exports = {
     images: {
-      domains: ['i.ibb.co', 's3-eu-west-1.amazonaws.com'],
       formats: ['image/avif', 'image/webp'],
       deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
       imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-      
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'i.ibb.co',
+          pathname: '/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 's3-eu-west-1.amazonaws.com',
+          pathname: '/**',
+        },
+      ],
     },
 
    
