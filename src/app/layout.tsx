cd0f21e577f89449638dc83e6da3ae0aa4d1c2c6
Oleksii Rozhko <rozhkoaleks@gmail.com>
2025-03-31 17:34:32 +0300

    Add @vercel/speed-insights dependency and integrate into layout component
    - Include @vercel/speed-insights in package.json and pnpm-lock.yaml for performance insights.
    - Integrate SpeedInsights component into the RootLayout for enhanced performance monitoring.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { ToastContainer } from "react-toastify";
 import { Jost } from "next/font/google";
 import AppProvider from "@/contextApi/AppProvider";
 import ReduxProvider from "@/redux/provider";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const jost = Jost({
   subsets: ["latin"],
@@ -13,41 +14,40 @@ const jost = Jost({
 
 export default function RootLayout({ children }: childrenType) {
   return (
-    <>
-      <html lang="en" className={jost.className}>
-        <head>
-          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-          <meta
-            name="description"
-            content="East European Food Store | Cary, NC | Goldenhex.com"
-          />
-          <meta name="robots" content="noindex, follow" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-          />
-          <title>East European Food Store | Cary, NC | Goldenhex.com</title>
-          <link rel="icon" href="/favicon.png" />
-        </head>
+    <html lang="en" className={jost.className}>
+      <head>
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="description"
+          content="East European Food Store | Cary, NC | Goldenhex.com"
+        />
+        <meta name="robots" content="noindex, follow" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <title>East European Food Store | Cary, NC | Goldenhex.com</title>
+        <link rel="icon" href="/favicon.png" />
+      </head>
 
-        <body suppressHydrationWarning={true}>
-          <ReduxProvider>
-            <AppProvider>{children}</AppProvider>
-            <ToastContainer
-              position="top-left"
-              autoClose={1000}
-              hideProgressBar={false}
-              newestOnTop={false}
-              closeOnClick
-              rtl={false}
-              pauseOnFocusLoss
-              draggable
-              pauseOnHover
-              theme="light"
-            />
-          </ReduxProvider>
-        </body>
-      </html>
-    </>
+      <body suppressHydrationWarning={true}>
+        <SpeedInsights />
+        <ReduxProvider>
+          <AppProvider>{children}</AppProvider>
+          <ToastContainer
+            position="top-left"
+            autoClose={1000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="light"
+          />
+        </ReduxProvider>
+      </body>
+    </html>
   );
 }
