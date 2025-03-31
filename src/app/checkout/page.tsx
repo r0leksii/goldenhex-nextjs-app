5211c7a13d980b28752c5455a28505182386e058
Oleksii Rozhko <rozhkoaleks@gmail.com>
2025-04-01 01:23:28 +0300

    Comment out CheckOutMain component in the checkout page for future use, enhancing code clarity and maintainability.

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import CheckOutMain from "@/components/checkout/CheckOutMain";
+// import CheckOutMain from "@/components/checkout/CheckOutMain";
 import Wrapper from "@/layout/DefaultWrapper";
 import PrivetRoute from "@/privetRoute/PrivetRoute";
 import { Elements } from "@stripe/react-stripe-js";
@@ -10,15 +10,13 @@ const stripePromise = loadStripe(`${process.env.STRIPE_PROMISE_KEY}`);
 const Checkout = () => {
   return (
     <>
-     <PrivetRoute>
-     <Wrapper>
-        <main>
-          <Elements stripe={stripePromise}>
-            <CheckOutMain />
-          </Elements>
-        </main>
-      </Wrapper>
-     </PrivetRoute>
+      <PrivetRoute>
+        <Wrapper>
+          <main>
+            <Elements stripe={stripePromise}>{/* <CheckOutMain /> */}</Elements>
+          </main>
+        </Wrapper>
+      </PrivetRoute>
     </>
   );
 };
