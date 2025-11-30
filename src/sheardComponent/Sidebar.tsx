0eec5cf69ece2ef5f40dba6af24e9f595d8de288
Oleksii Rozhko <rozhkoaleks@gmail.com>
2025-11-30 18:17:24 +0200

    refactor: modernize sidebar with improved layout, styling, and accessibility
    Restructure sidebar component with cleaner semantic HTML and improved spacing. Remove unused cart and wishlist icon imports. Add aria-labels to interactive elements for better accessibility. Enhance contact info section with simplified address display and proper link formatting.
    
    Update offcanvas styles with modern design system featuring rounded corners, softer colors, and smooth transitions. Implement consistent spacing with

--- src/sheardComponent/Sidebar.tsx
+++ src/sheardComponent/Sidebar.tsx
@@ -1,12 +1,11 @@
 import Link from "next/link";
 import React from "react";
 import useGlobalContext from "../hooks/use-context";
-import CartIcon from "../sheardComponent/elements/icons/cart-icon";
-import WishlistIcon from "../sheardComponent/elements/icons/wishlist-icon";
 import logo from "../../public/assets/img/logo/logo-h-goldenhex.png";
 import Image from "next/image";
 import MobileMenu from "./elements/MobileMenu";
 import SidebarSearchContent from "./elements/SidebarSearchContent";
+
 const Sidebar = () => {
   const { showSidebar, setShowSidebar } = useGlobalContext();
   const safeSetShowSidebar = setShowSidebar || (() => {});
@@ -19,92 +18,77 @@ const Sidebar = () => {
             <div className="modals-content">
               <div className="offcanvas__wrapper">
                 <div className="offcanvas__content">
-                  <div className="offcanvas__top mb-40">
-                    <div className="offcanvas__logo logo">
+                  {/* Header */}
+                  <div className="offcanvas__top">
+                    <div className="offcanvas__logo">
                       <Link href="/">
-                        <Image src={logo} alt="logo" width={180} height={70} />
+                        <Image src={logo} alt="GoldenHex" width={160} height={50} />
                       </Link>
                     </div>
                     <div className="offcanvas__close">
                       <button
                         className="offcanvas__close-btn"
                         onClick={() => safeSetShowSidebar(false)}
+                        aria-label="Close menu"
                       >
                         <i className="fal fa-times"></i>
                       </button>
                     </div>
                   </div>
-                  {/* <div className="bd-utilize__buttons mb-25 d-none">
-                    <div className="bd-action__item">
-                      <div className="bd-action__cart d-none">
-                        <div className="bd-action__cart-icon">
-                          <span className="bd-cart-mini-btn">
-                            <CartIcon />
-                          </span>
-                          <span className="bd-action__item-number cart-count">
-                            0
-                          </span>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="bd-action__item d-none">
-                      <div className="bd-action__wishlist">
-                        <div className="bd-action__wistlist-icon">
-                          <span className="bd-cart-mini-btn">
-                            <WishlistIcon />
-                          </span>
-                          <span className="bd-action__item-number wishlist-count">
-                            0
-                          </span>
-                        </div>
-                      </div>
-                    </div>
-                  </div> */}
-                  <div className="offcanvas__search mb-25">
+
+                  {/* Search */}
+                  <div className="offcanvas__search">
                     <SidebarSearchContent />
                   </div>
+
+                  {/* Mobile Navigation */}
                   <nav className="side-mobile-menu d-block d-xl-none mm-menu">
                     <MobileMenu />
                   </nav>
-                  <div className="offcanvas__contact mt-30 mb-20">
+
+                  {/* Contact Info */}
+                  <div className="offcanvas__contact">
                     <h4>Contact Info</h4>
                     <ul>
-                      <li className="d-flex align-items-center">
-                        <div className="offcanvas__contact-icon mr-15">
+                      <li>
+                        <div className="offcanvas__contact-icon">
                           <i className="fal fa-map-marker-alt"></i>
                         </div>
                         <div className="offcanvas__contact-text">
                           <Link target="_blank" href="https://g.co/kgs/egQkRSZ">
-                            1200 NW Maynard Rd Cary, North Carolina
+                            1200 NW Maynard Rd, Cary, NC
                           </Link>
                         </div>
                       </li>
-                      <li className="d-flex align-items-center">
-                        <div className="offcanvas__contact-icon mr-15">
+                      <li>
+                        <div className="offcanvas__contact-icon">
                           <i className="far fa-phone"></i>
                         </div>
                         <div className="offcanvas__contact-text">
                           <Link href="tel:+19199462649">(919) 946-2649</Link>
                         </div>
                       </li>
-                      <li className="d-flex align-items-center">
-                        <div className="offcanvas__contact-icon mr-15">
+                      <li>
+                        <div className="offcanvas__contact-icon">
                           <i className="fal fa-envelope"></i>
                         </div>
                         <div className="offcanvas__contact-text">
                           <Link href="mailto:sales@goldenhex.com">
-                            <span>sales@goldenhex.com</span>
+                            sales@goldenhex.com
                           </Link>
                         </div>
                       </li>
                     </ul>
                   </div>
+
+                  {/* Social Links */}
                   <div className="offcanvas__social">
                     <ul>
                       <li>
                         <Link
                           href="https://www.facebook.com/GoldenHex/"
                           target="_blank"
+                          aria-label="Facebook"
                         >
                           <i className="fab fa-facebook-f"></i>
                         </Link>
@@ -113,7 +97,7 @@ const Sidebar = () => {
                         <Link
                           href="https://www.instagram.com/goldenhexfoods/"
                           target="_blank"
-                          title="Instagram"
+                          aria-label="Instagram"
                         >
                           <i className="fab fa-instagram"></i>
                         </Link>
